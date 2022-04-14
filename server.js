3f08538b7b9ec95686281c45cfcb2d560a62de34
thong dang <duythong2708@gmail.com>
2022-04-15 00:00:20 +0300

    changes

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ let express = require('express');
 let app = express();
 let cors = require('cors');
 let path = require('path');
-
+let pool = require('./database')
 let pathView = path.join(__dirname,'./public');
 app.use(cors());
 app.get('/fruits', async function (req,res) {
 
-    res.status(200).json({first_name_patient : 'Thong Dang'})
+    // res.status(200).json([{first_name_patient : 'Thong Dang'}])
     
-    // let {rows} = await pool.query("Select * from patient");
+    let {rows} = await pool.query("Select * from patient");
 
-    // res.status(200).json(rows);
+    res.status(200).json(rows);
 })
 
 app.get('/page', function (req,res) {
@@ -20,7 +20,7 @@ app.get('/page', function (req,res) {
     // res.json(["Sol7"]);
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log("Server is running!");
 })
