a4fd0308657de86adcf3f7179e383b7da8b05a45
thong dang <duythong2708@gmail.com>
2022-04-14 22:01:24 +0300

    test heroku 2

---
+++ server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+// const pool = require('../testHeroku/database');
+let app = express();
+
+let path = require('path');
+
+let pathView = path.join(__dirname,'../public');
+
+// app.get('/fruits', async function (req,res) {
+    
+//     let {rows} = await pool.query("Select * from patient");
+
+//     res.status(200).json(rows);
+// })
+
+app.get('/page', function (req,res) {
+    // res.sendFile(pathView + '/testHTML.html');
+    res.json(["Sol7"]);
+})
+
+const port = 3000;
+app.listen(port, function () {
+    console.log("Server is running!");
+})
