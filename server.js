ee250a127a80bd8b22bbe9113adc866c3278046e
thong dang <duythong2708@gmail.com>
2022-04-14 22:17:32 +0300

    checked

--- server.js
+++ server.js
@@ -6,19 +6,21 @@ let path = require('path');
 
 let pathView = path.join(__dirname,'../public');
 
-// app.get('/fruits', async function (req,res) {
+app.get('/fruits', async function (req,res) {
+
+    res.status(200).json({first_name_patient : 'Thong Dang'})
     
-//     let {rows} = await pool.query("Select * from patient");
+    // let {rows} = await pool.query("Select * from patient");
 
-//     res.status(200).json(rows);
-// })
-
-app.get('/page', function (req,res) {
-    // res.sendFile(pathView + '/testHTML.html');
-    res.json(["Sol7"]);
+    // res.status(200).json(rows);
 })
 
-const port = process.env.PORT || 8080;
+app.get('/page', function (req,res) {
+    res.sendFile(pathView + '/testHTML.html');
+    // res.json(["Sol7"]);
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Server is running!");
 })
