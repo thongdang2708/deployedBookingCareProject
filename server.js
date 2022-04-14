22bd2f9757ce32747f9be5424f122a70e394692c
thong dang <duythong2708@gmail.com>
2022-04-15 00:07:58 +0300

    check

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/page', function (req,res) {
     // res.json(["Sol7"]);
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log("Server is running!");
 })
